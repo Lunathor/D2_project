eb5d4129bc3e383989a530de4589245b6ceda545
Lunathor <mrcoopgmz@gmail.com>
2024-04-25 17:00:08 +0300

    Улучшено DetailedView для Каждой отдельно взятой новости

--- NewsPaper/NewsPortal/views.py
+++ NewsPaper/NewsPortal/views.py
@@ -7,7 +7,6 @@ from .models import Post
 class NewsList(ListView):
     model = Post
     ordering = '-dateCreation'
-    # queryset = Post.objects.order_by('title').values('title')
     template_name = 'news.html'
     context_object_name = 'news'
     

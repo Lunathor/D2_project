416dfdeb1bbf491bbfd6f9b88114729c649a8182
Lunathor <mrcoopgmz@gmail.com>
2024-04-25 13:47:37 +0300

    Добавлен табличный вывод новостей

--- NewsPaper/NewsPortal/views.py
+++ NewsPaper/NewsPortal/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.views.generic import ListView, DetailView
 from .models import Post
 
@@ -8,6 +10,11 @@ class NewsList(ListView):
     # queryset = Post.objects.order_by('title').values('title')
     template_name = 'news.html'
     context_object_name = 'news'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
 
 
 class PostDetail(DetailView):

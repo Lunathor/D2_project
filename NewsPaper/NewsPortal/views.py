6ba4f460daeb6870ae1dddcd8d637c8505c2a87b
Lunathor <mrcoopgmz@gmail.com>
2024-04-27 12:46:39 +0300

    Добавлены фильтры и теги, упразднен метод get_context_data в views.py и заменен на фильтр current_time. Создан фильтр цензурирующий, не желательный контекст (сейчас для примера написано содержание одной из статей)

--- NewsPaper/NewsPortal/views.py
+++ NewsPaper/NewsPortal/views.py
@@ -10,12 +10,7 @@ class NewsList(ListView):
     template_name = 'news.html'
     context_object_name = 'news'
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['time_now'] = datetime.utcnow()
-        return context
-
-
+    
 class PostDetail(DetailView):
     model = Post
     template_name = 'post.html'

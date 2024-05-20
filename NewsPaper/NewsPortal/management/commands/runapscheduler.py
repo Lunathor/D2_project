bf84f28bbdc00cad80867da909a204cfbcfd18bc
Lunathor <mrcoopgmz@gmail.com>
2024-05-20 14:07:14 +0300

    Доделан runapscheduler.py

--- NewsPaper/NewsPortal/management/commands/runapscheduler.py
+++ NewsPaper/NewsPortal/management/commands/runapscheduler.py
@@ -1,24 +1,46 @@
+import datetime
 import logging
+from datetime import timezone
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django.conf import settings
-from django.core.mail import mail_managers
+from django.core.mail import mail_managers, EmailMultiAlternatives
 from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
 from django_apscheduler import util
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 
-from NewsPortal.models import Post
+from NewsPortal.models import Post, Subscriber
 
 logger = logging.getLogger(__name__)
 
 
 def my_job():
-    posts = Post.objects.order_by('dateCreation')[:3]
-    text = '\n'.join(['{} - {}'.format(p.title, p.dateCreation) for p in posts])
-    mail_managers("Посты за последнюю неделю", text)
-
+    today = timezone.now()
+    last_week = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(dateCreation__gte=last_week)
+    categories = set(posts.values_list('postCategory__name', flat=True))
+    subscribers = Subscriber.objects.filter(category__name__in=categories)
+    
+    for sub in subscribers:
+        html_content = render_to_string(
+            'weekly_posts.html',
+            {
+                'link': 'http://127.0.0.1:8000/',
+                'posts': posts,
+                'user': sub.user.username
+            }
+        )
+        msg = EmailMultiAlternatives(
+            subject='Посты за неделю',
+            body='',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=[sub.user.email],
+        )
+        msg.attach_alternative(html_content, 'text/html')
+        msg.send
 
 @util.close_old_connections
 def delete_old_job_executions(max_age=604_800):

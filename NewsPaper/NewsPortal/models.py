052175329f15b04713eddc04508abc97aab94921
Lunathor <mrcoopgmz@gmail.com>
2024-06-26 20:12:42 +0300

    Админка стала чуть более удобной

--- NewsPaper/NewsPortal/models.py
+++ NewsPaper/NewsPortal/models.py
@@ -21,14 +21,13 @@ class Author(models.Model):
         
         self.ratingAuthor = pRate * 3 + cRate
         self.save()
-        
+    
     def __str__(self):
         return self.Author_to_user.username
 
 
 class Category(models.Model):
     name = models.CharField(max_length=64, unique=True)
-    
     
     def __str__(self):
         return self.name
